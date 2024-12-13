ec8ce956d8b9b0ddf4b88872117086f0042e9af1
tic-top <kirp@umich.edu>
2024-12-13 15:02:30 +0800

    diff

---
+++ pard.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# (See https://arc-ts.umich.edu/greatlakes/user-guide/ for command details)
+# Set up batch job settings
+
+#SBATCH --job-name=sudoku
+#SBATCH --mail-type=BEGIN,END
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=4
+#SBATCH --mem-per-cpu=1g
+#SBATCH --time=00:05:00
+#SBATCH --account=cse587f24_class
+#SBATCH --partition=standard
+
+module load gcc
+module load openmpi
+mpic++ -O3 parallel.cpp -o main
+export UCX_LOG_LEVEL=error
+mpirun -n 4 ./main 000000010400000000020000000000050407008000300001090000300400200050100000000806000
+
+    
