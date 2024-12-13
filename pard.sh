654e87d89583d8d69179a188d89fdef0e4282bdd
tic-top <kirp@umich.edu>
2024-12-13 22:33:08 +0800

    hi

--- pard.sh
+++ pard.sh
@@ -5,7 +5,7 @@
 #SBATCH --job-name=sudoku
 #SBATCH --mail-type=BEGIN,END
 #SBATCH --nodes=1
-#SBATCH --ntasks-per-node=4
+#SBATCH --ntasks-per-node=16
 #SBATCH --mem-per-cpu=1g
 #SBATCH --time=00:05:00
 #SBATCH --account=cse587f24_class
@@ -16,6 +16,7 @@ module load openmpi
 mpic++ -O3 parallel.cpp -o main
 export UCX_LOG_LEVEL=error
 # echo 000000010400000000020000000000050407008000300001090000300400200050100000000806000 | mpirun -n 4 ./main
-echo 061490020280007050003108007600704031000250074090600000000010008570000206800906000 | mpirun -n 4 ./main
+echo "pard" >> result.txt
+echo 000070030000003020056200089000064001007000000090080005900005000000037910280000000 | mpirun -n 16 ./main >> result.txt
 
     
