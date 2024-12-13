cb9bf5cfff3737d45c66e664525333baf782204b
tic-top <kirp@umich.edu>
2024-12-13 15:43:15 +0800

    updat

--- parallel.cpp
+++ parallel.cpp
@@ -91,6 +91,7 @@ vector<vector<int>> expandNode(const vector<int> &board) {
 
 void ensureEnoughTasks(queue<vector<int>> &tasks, int p) {
     while ((int)tasks.size() < p+1) {
+        cout << "taks num: " << tasks.size() << endl;
         if (tasks.empty()) return;
         vector<int> front = tasks.front();
         tasks.pop();
