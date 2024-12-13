09724261441459a3d0d4adf12c99f851d283f15a
tic-top <kirp@umich.edu>
2024-12-13 13:09:24 +0800

    [kirp] only one node

--- parallel.cpp
+++ parallel.cpp
@@ -191,7 +191,7 @@ int main(int argc, char* argv[]) {
             } else if (tag == TAG_SOLUTION_FAIL) {
                 int dummy;
                 MPI_Recv(&dummy, 1, MPI_INT, source, TAG_SOLUTION_FAIL, MPI_COMM_WORLD, &status);
-                ensureEnoughTasks(tasks, p);
+                // ensureEnoughTasks(tasks, p);
                 if (tasks.empty()) {
                     MPI_Send(NULL, 0, MPI_INT, source, TAG_NO_MORE_TASK, MPI_COMM_WORLD);
                     activeWorkers--;
@@ -202,7 +202,7 @@ int main(int argc, char* argv[]) {
             } else if (tag == TAG_REQUEST_TASK) {
                 int dummy;
                 MPI_Recv(&dummy, 1, MPI_INT, source, TAG_REQUEST_TASK, MPI_COMM_WORLD, &status);
-                ensureEnoughTasks(tasks, p);
+                // ensureEnoughTasks(tasks, p);
                 if (tasks.empty()) {
                     MPI_Send(NULL, 0, MPI_INT, source, TAG_NO_MORE_TASK, MPI_COMM_WORLD);
                     activeWorkers--;
