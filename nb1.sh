d6b12883d4dab375d37c83cea31d97b9d3b8e515
tic-top <kirp@umich.edu>
2024-12-14 01:36:08 +0800

    h

---
+++ nb1.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#SBATCH --job-name=sudoku
+#SBATCH --mail-type=BEGIN,END
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --mem-per-cpu=1g
+#SBATCH --time=00:05:00
+#SBATCH --account=cse587f24_class
+#SBATCH --partition=standard
+
+output_csv="output1.csv"
+./serial.exe < sudoku.csv > "$output_csv"
