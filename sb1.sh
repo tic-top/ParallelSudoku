fbefa838de8a8ac2f1fb27bd8f4322475a5a8ec3
tic-top <kirp@umich.edu>
2024-12-13 22:10:14 +0800

    update

--- sb1.sh
+++ sb1.sh
@@ -7,7 +7,7 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --mem-per-cpu=1g
-#SBATCH --time=00:5:00
+#SBATCH --time=00:05:00
 #SBATCH --account=cse587f24_class
 #SBATCH --partition=standard
 
--- sb32.sh
+++ sb32.sh
@@ -7,7 +7,7 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=32
 #SBATCH --mem-per-cpu=1g
-#SBATCH --time=00:10:00
+#SBATCH --time=00:05:00
 #SBATCH --account=cse587f24_class
 #SBATCH --partition=standard
 
